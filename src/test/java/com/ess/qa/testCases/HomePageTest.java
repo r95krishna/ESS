eac6696c3107473f137b7c8259dcfaf4d9bb1203
r95krishna <r95krishna@gmail.com>
2018-09-03 19:21:09 +0530

    new changes in categoy

---
+++ src/test/java/com/ess/qa/testCases/HomePageTest.java
@@ -0,0 +1,91 @@
+package com.ess.qa.testCases;
+
+import com.ess.qa.base.TestBase;
+import com.ess.qa.pages.HomePage;
+import com.ess.qa.pages.LoginPage;
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomePageTest extends TestBase {
+
+    LoginPage loginPage;
+    HomePage homePage;
+    LoginPageTest loginPageTest;
+
+
+    @BeforeMethod
+    public void setup() {
+        initialisation();
+        loginPage = new LoginPage();
+        homePage = new HomePage();
+        loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    }
+
+
+    @Test(priority = 1)
+    public void SwitchEssButtonTest() {
+
+        //    loginPageTest.LoginTest();
+
+        if (homePage.ValidateSwithEssButton()) {
+            homePage.ValidateSwitchEss();
+        } else {
+            System.out.println("Allready in the new Ess Portal");
+        }
+    }
+
+    @Test(priority = 2)
+    public void WorkFlowStatusTest() throws InterruptedException {
+        homePage.HelpdeskTabClick();
+        driver.findElement(By.xpath("//span[@class='label-name ng-star-inserted']")).click();
+        Thread.sleep(5000);
+        String ws = homePage.ValidateWorkflowStatus();
+        System.out.println(ws);
+        Assert.assertEquals(ws, "ACTIVE");
+    }
+
+    @Test(priority = 3)
+    public void ValidateNewRequestTest() throws InterruptedException {
+        homePage.HelpdeskTabClick();
+        Thread.sleep(5000);
+        homePage.ValidateNewRequest();
+        //  homePage.ValidateCategory();
+    }
+
+    @Test(priority = 4)
+    public void ValidateNewRequest() throws InterruptedException {
+
+        homePage.HelpdeskTabClick();
+        Thread.sleep(5000);
+        homePage.ValidateNewRequest();
+
+        List<WebElement> list = driver.findElements(By.xpath("//ul[@class='dropdown-menu ng-star-inserted']/li/a"));
+
+        for (WebElement elm : list) {
+            System.out.println("values :" + elm.getAttribute("title"));
+
+
+            if (elm.getAttribute("title").contains("New Agent login ID needs to create .")) {
+                elm.click();
+
+
+                break;
+            }
+
+          /*  driver.findElement(By.xpath("//input[@id='subject']")).sendKeys("subject");
+            driver.findElement(By.xpath("//textarea[@id='comment']")).sendKeys("Description");
+
+
+            driver.findElement(By.xpath("//i[@class='icon-gt-attachment icon-link text-bold']")).sendKeys("C:\\Users\\Rahul Krishna\\Pictures\\Help desk\\Capture.png");
+*/
+
+        }
+
+    }
+}
